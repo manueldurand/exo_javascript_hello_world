1383152b01e9b1e5d4a8ad2b297be22a56e1ad4b
manuel_durand <manuel.durand@mailo.com>
2022-11-24 01:08:24 +0100

    corrections

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function affiche(){
     document.write("HELLO WORLD !");
 }
+
+// correction : 
+// let btnBonjour = document.querySelector(".btn-boujour");
+// btnBonjour.addEventListener("click", (e) => {
+//     alert("Hello World !");
+// });
